58c94d8ca9b12b2d9fa3b3d984a980a2c38dd6c1
jakobN1 <e11908534@student.tuwien.ac.at>
2025-01-26 17:31:25 +0100

    added sql queries to result doc/pdf

--- BI_Projects/BI_59/task3/3_1.sql
+++ BI_Projects/BI_59/task3/3_1.sql
@@ -1,5 +1,7 @@
-select ProductTopCategoryName, sum(OrderLineProfit) from Dim_Product p
+select ProductTopCategoryName as 'Product Top Category', 
+sum(OrderLineProfit) as Profit
+from Dim_Product p
 join Fact_InternetSales f on f.ProductKey=p.ProductKey
 where year(OrderDate) = 2021
 group by ProductTopCategoryName
-order by sum(OrderLineProfit) desc
+order by sum(OrderLineProfit) desc;
