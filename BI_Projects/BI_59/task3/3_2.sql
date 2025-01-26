a1b7698fba04d4ae24ef35d6508da06c0ec3ea00
rsperlich <raphael.sperlich@gmail.com>
2025-01-26 10:33:16 +0100

    add final comments and stuff

--- BI_Projects/BI_59/task3/3_2.sql
+++
@@ -1,6 +0,0 @@
-select l.Country, sum(OrderLineTotal)
-from Dim_Location l
-join Fact_InternetSales f on f.ShipToLocationKey=l.LocationKey
-where year(OrderDate) = 2019
-group by l.Country
-order by sum(OrderLineTotal) desc
